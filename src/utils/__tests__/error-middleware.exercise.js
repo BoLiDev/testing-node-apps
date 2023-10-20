ebe09c0b85c3ebba150585744cd75b2ec173aebf
Bo Li <libodev97@gmail.com>
2023-10-20 09:03:15 +0800

    test middleware

--- src/utils/__tests__/error-middleware.exercise.js
+++ src/utils/__tests__/error-middleware.exercise.js
@@ -1,16 +1,42 @@
 // Testing Middleware
 
-// 💣 remove this todo test (it's only here so you don't get an error about missing tests)
-test.todo('remove me')
-
 // 🐨 you'll need both of these:
 // import {UnauthorizedError} from 'express-jwt'
 // import errorMiddleware from '../error-middleware'
+import {UnauthorizedError} from 'express-jwt'
+import errorMiddleware from 'utils/error-middleware'
+import {buildNext, buildReq, buildRes} from 'utils/generate'
 
-// 🐨 Write a test for the UnauthorizedError case
-// 💰 const error = new UnauthorizedError('some_error_code', {message: 'Some message'})
-// 💰 const res = {json: jest.fn(() => res), status: jest.fn(() => res)}
+test('headersSent case', () => {
+  const res = buildRes()
+  const next = buildNext()
+  const req = buildReq()
+  const code = 'auth_error_code'
+  const message = 'auth error message'
+  const error = new UnauthorizedError(code, {message})
+  errorMiddleware(error, req, res, next)
+  expect(next).not.toHaveBeenCalled()
+  expect(res.status).toHaveBeenCalledWith(401)
+  expect(res.status).toHaveBeenCalledTimes(1)
+  expect(res.json).toHaveBeenCalledWith({
+    code,
+    message,
+  })
+  expect(res.status).toHaveBeenCalledTimes(1)
+})
 
-// 🐨 Write a test for the headersSent case
-
-// 🐨 Write a test for the else case (responds with a 500)
+test('else case', () => {
+  const res = buildRes()
+  const next = buildNext()
+  const req = buildReq()
+  const error = new Error('else_error_code')
+  errorMiddleware(error, req, res, next)
+  expect(next).not.toHaveBeenCalled()
+  expect(res.status).toHaveBeenCalledWith(500)
+  expect(res.status).toHaveBeenCalledTimes(1)
+  expect(res.json).toHaveBeenCalledWith({
+    message: error.message,
+    stack: error.stack,
+  })
+  expect(res.json).toHaveBeenCalledTimes(1)
+})
